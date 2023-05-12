221341987f1ebad11bf061a6ef9f6a2a0c817a33
Adrian Videnie <avidenie@gmail.com>
2023-05-12 21:58:26 +0300

    test: tests for popular posts controller

--- test/features/posts/data/repositories/post_repository_impl_test.dart
+++ test/features/posts/data/repositories/post_repository_impl_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rocket/features/posts/data/data_sources/post_data_source.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
+import 'package:rocket/features/posts/data/mappers/post_to_domain.dart';
+import 'package:rocket/features/posts/data/models/post_dto.dart';
 import 'package:rocket/features/posts/data/repositories/post_repository_impl.dart';
 
 import 'post_repository_impl_test.mocks.dart';
@@ -29,6 +31,58 @@ void main() {
         // assert
         verify(mockPostDataSource.getPopularPosts());
         expect(result, equals([]));
+      },
+    );
+
+    test(
+      'returns a list of posts loaded by the data source',
+      () async {
+        // ARRANGE
+        // create a list of posts
+        const tPost1 = PostDto(
+            title: 'lorem ipsum 1', author: 'author 1', subreddit: 'news');
+        const tPost2 = PostDto(
+            title: 'lorem ipsum 2', author: 'author 2', subreddit: 'news');
+        final tPosts = [tPost1, tPost2];
+
+        // stub the data source response
+        when(mockPostDataSource.getPopularPosts())
+            .thenAnswer((_) async => Future.value(tPosts));
+
+        // ACT
+        final result = await repository.getPopularPosts();
+
+        // ASSERT
+        verify(mockPostDataSource.getPopularPosts()).called(1);
+        expect(result, tPosts.map((post) => post.toDomain()));
+      },
+    );
+
+    test(
+      'throws an error when the data source throws an error',
+      () async {
+        // ARRANGE
+        // create an exception
+        final error = Exception('some error');
+
+        // stub the data source response
+        when(mockPostDataSource.getPopularPosts()).thenThrow(error);
+
+        // ACT
+        final call = repository.getPopularPosts;
+
+        // ASSERT
+        await expectLater(
+          call,
+          throwsA(
+            isA<Exception>().having(
+              (err) => err.toString(),
+              'toString()',
+              'Exception: some error',
+            ),
+          ),
+        );
+        verify(mockPostDataSource.getPopularPosts()).called(1);
       },
     );
   });
---
+++ test/features/posts/presentation/controllers/popular_posts_controller_test.dart
@@ -0,0 +1,133 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rocket/features/posts/data/repositories/post_repository_impl.dart';
+import 'package:rocket/features/posts/domain/models/post.dart';
+import 'package:rocket/features/posts/domain/repositories/post_repository.dart';
+import 'package:rocket/features/posts/presentation/controllers/popular_posts_controller.dart';
+
+import 'popular_posts_controller_test.mocks.dart';
+
+class Listener<T> extends Mock {
+  void call(T? previous, T? next);
+}
+
+@GenerateNiceMocks([MockSpec<PostRepository>()])
+void main() {
+  // A helper method to create a ProviderContainer that overrides the post
+  // repository provider.
+  ProviderContainer setupContainer(MockPostRepository postRepository) {
+    final container = ProviderContainer(
+      overrides: [
+        postRepositoryProvider.overrideWithValue(postRepository),
+      ],
+    );
+    addTearDown(container.dispose);
+    return container;
+  }
+
+  test('initial state is AsyncLoading', () {
+    // arrange
+    final postRepository = MockPostRepository();
+    final container = setupContainer(postRepository);
+    final listener = Listener<AsyncValue<List<Post>>>();
+
+    // act
+    container.listen(
+      popularPostsControllerProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    // assert
+    verify(listener(null, const AsyncLoading<List<Post>>()));
+    verifyNoMoreInteractions(listener);
+  });
+
+  test('state is AsyncData when loading a list of popular posts', () async {
+    // ARRANGE
+    // create a list of posts
+    const tPost1 =
+        Post(title: 'lorem ipsum 1', author: 'author 1', community: 'news');
+    const tPost2 =
+        Post(title: 'lorem ipsum 2', author: 'author 2', community: 'news');
+    final tPosts = [tPost1, tPost2];
+
+    // create a mock repository
+    final postRepository = MockPostRepository();
+
+    // create the ProviderContainer with the mock repository
+    final container = setupContainer(postRepository);
+
+    // stub the repository response
+    when(postRepository.getPopularPosts())
+        .thenAnswer((_) => Future.value(tPosts));
+
+    // create a listener
+    final listener = Listener<AsyncValue<List<Post>>>();
+
+    // listen to the provider and call listener whenever its value changes
+    container.listen(
+      popularPostsControllerProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    // ACT
+    var popularPosts =
+        await container.read(popularPostsControllerProvider.future);
+
+    // ASSERT
+    verifyInOrder([
+      listener(null, const AsyncLoading<List<Post>>()),
+      listener(const AsyncLoading<List<Post>>(), AsyncData<List<Post>>(tPosts)),
+    ]);
+
+    verify(postRepository.getPopularPosts()).called(1);
+    verifyNoMoreInteractions(listener);
+
+    expect(popularPosts, tPosts);
+  });
+
+  test(
+      'state is AsyncError when an error occurs '
+      'while loading the list of popular posts', () async {
+    // ARRANGE
+    // create a mock repository
+    final postRepository = MockPostRepository();
+
+    // create the ProviderContainer with the mock repository
+    final container = setupContainer(postRepository);
+
+    // create an exception
+    final exception = Exception('Some error message');
+
+    // stub the repository response
+    when(postRepository.getPopularPosts()).thenThrow(exception);
+
+    // create a listener
+    final listener = Listener<AsyncValue<List<Post>>>();
+
+    // listen to the provider and call listener whenever its value changes
+    container.listen(
+      popularPostsControllerProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    // ACT
+    final call = container.read(popularPostsControllerProvider.future);
+
+    // ASSERT
+    await expectLater(call, throwsA(isA<Exception>()));
+
+    verifyInOrder([
+      listener(null, const AsyncLoading<List<Post>>()),
+      listener(const AsyncLoading<List<Post>>(), argThat(isA<AsyncError>())),
+    ]);
+
+    verify(postRepository.getPopularPosts()).called(1);
+    verifyNoMoreInteractions(listener);
+  });
+}
