3f76941b42cc9b857f7c38446dd6832101ef16e1
Adrian Videnie <avidenie@gmail.com>
2023-05-19 23:20:22 +0300

    refactor: split post card in multiple widgets

--- lib/features/posts/presentation/widgets/post_card.dart
+++ lib/features/posts/presentation/widgets/post_card.dart
@@ -10,29 +10,57 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            dense: true,
-            leading: const CircleAvatar(),
-            title: Text(post.author),
-            subtitle: Text(post.community),
-          ),
-          SafeArea(
-            minimum: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-            top: false,
-            bottom: false,
-            child: Text(
-              post.title,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium
-                  ?.copyWith(height: 1.4),
-            ),
-          )
-        ],
+      child: _Content(post: post),
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({required this.post});
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _Heading(post: post),
+        _Title(post: post),
+      ],
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({required this.post});
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+      child: Text(
+        post.title,
+        style: Theme.of(context).textTheme.titleMedium?.copyWith(height: 1.4),
       ),
+    );
+  }
+}
+
+class _Heading extends StatelessWidget {
+  const _Heading({required this.post});
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      dense: true,
+      leading: const CircleAvatar(),
+      title: Text(post.author),
+      subtitle: Text(post.community),
     );
   }
 }
